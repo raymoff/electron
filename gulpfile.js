9998f89849538e378c1f4cd54abd89778f8f5ad0
raymoff <uraimoff@yahoo.com>
2020-04-08 03:10:41 +0300

    fonts import

--- gulpfile.js
+++ gulpfile.js
@@ -24,7 +24,7 @@ gulp.task('browser-sync', function() {
 			baseDir: 'app'
 		},
 		notify: false,
-		// open: false,
+		open: false,
 		// online: false, // Work Offline Without Internet Connection
 		// tunnel: true, tunnel: "projectname", // Demonstration page: http://projectname.localtunnel.me
 	})
